0c02abf28d8a486900f8b51ce2624812029a7d05
“T3sultan” <incognito.soul011@gmail.com>
2022-04-15 03:20:06 +0600

    fonts-add

---
+++ src/theme/fonts.js
@@ -0,0 +1,16 @@
+const type = {
+  nunitoBlack: "NunitoBlack",
+  nunitoBold: "NunitoBold",
+};
+const size = {
+  small: 10,
+  caption: 12,
+  body: 14,
+  header: 16,
+  title: 18,
+  headLine: 20,
+  display: 22,
+  jumbo: 26,
+  mega: 30,
+};
+export default { type, size };
--- src/theme/index.js
+++ src/theme/index.js
@@ -1,4 +1,5 @@
 import Colors from "./colors";
 import Metrics from "./metrics";
+import Fonts from "./fonts";
 
-export { Colors, Metrics };
+export { Colors, Metrics, Fonts };
