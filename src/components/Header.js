601587dfeb9a2229e4dc1204fe966867f1970fd2
“T3sultan” <incognito.soul011@gmail.com>
2022-04-26 10:12:53 +0600

    updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import React from "react";
-import { Metrics, Images } from "../theme";
+import { Metrics, Images, Colors } from "../theme";
 
 const Header = ({ navigation }) => {
   return (
